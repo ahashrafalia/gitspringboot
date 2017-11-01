26209ccbd3d22a02e0e3a226fb639cfefd433176
ashrafgit <ahashrafalia@gmail.com>
2017-11-01 20:53:47 +0530

    oracle changes

--- gitspringboot/src/main/java/com/gitspringboot/model/HelloLog.java
+++ gitspringboot/src/main/java/com/gitspringboot/model/HelloLog.java
@@ -5,7 +5,9 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -14,23 +16,24 @@ import javax.validation.constraints.NotNull;
 public class HelloLog {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "HELLO_SEQ")
+    @SequenceGenerator(sequenceName = "hello_seq", allocationSize = 1, name = "HELLO_SEQ")
 	@NotNull
-	@Column(name="uid", unique=true)
+	@Column(name="uids", unique=true)
 	private int uid;
 	
-	@Column(name="name")
+	@Column(name="names")
 	private String name;
 	
 	@NotNull
-	@Column(name="timestamp")
-	private Date timestamp;
+	@Column(name="timestamps")
+	private Date timestamps;
 	
 	public HelloLog() {}
 	public HelloLog(String name) {
 		
 		this.name = name;
-		this.timestamp = new Date();
+		this.timestamps = new Date();
 	}
 	
 	
@@ -46,11 +49,11 @@ public class HelloLog {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Date getTimestamp() {
-		return timestamp;
+	public Date getTimestamps() {
+		return timestamps;
 	}
-	public void setTimestamp(Date timestamp) {
-		this.timestamp = timestamp;
+	public void setTimestamps(Date timestamp) {
+		this.timestamps = timestamp;
 	}
 	
 	
