6f4bbb66e90b14a3cc2357b4f0d2c8eb3d162d50
ashrafgit <ahashrafalia@gmail.com>
2017-12-10 21:50:16 +0530

    jboss changes

--- gitspringboot/src/main/java/com/gitspringboot/ArticalMain.java
+++ gitspringboot/src/main/java/com/gitspringboot/ArticalMain.java
@@ -2,14 +2,24 @@ package com.gitspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class ArticalMain {
+public class ArticalMain extends SpringBootServletInitializer {
+	 
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArticalMain.class, args);	
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(
+	    SpringApplicationBuilder application) {
+	    return application.sources(ArticalMain.class);
+	  }
 
 }
