9aeaf8b8e3d44ab46e4183c52e3c0204cca74b34
ashrafgit <ahashrafalia@gmail.com>
2018-02-15 19:18:43 +0530

    spring autowired

--- gitspringboot/src/main/java/com/gitspringboot/mvccontorller/CertMvcController.java
+++ gitspringboot/src/main/java/com/gitspringboot/mvccontorller/CertMvcController.java
@@ -1,16 +1,24 @@
 package com.gitspringboot.mvccontorller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.gitspringboot.zample.SuperInterface;
+
 @Controller
 public class CertMvcController {
 
+	@Autowired
+	SuperInterface superInterface;
+	
 	@PreAuthorize("hasAnyRole('ROLE_CLIENT')")
 	@RequestMapping(value="/certHomeClient",method=RequestMethod.GET)
 	public String index(){
+		
+		superInterface.show();
 		return "certHomeClient";
 	}
 	@PreAuthorize("hasAnyRole('ROLE_TECH')")
